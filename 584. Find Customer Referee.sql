416b15c0edf06412e3d4dd0453665b7261050be9
Abdallah Maher <abdallahmaher@Abdallahs-MacBook-Air.local>
2022-08-09 18:44:00 +0200

    584. Find Customer Referee

---
+++ 584. Find Customer Referee.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select name
+from Customer
+-- where referee_id is null || referee_id != 2
+-- where ifnull(referee_id, -1) <> 2 -- same time
+where coalesce(referee_id, -1) <> 2 -- better time is null and ifnull
