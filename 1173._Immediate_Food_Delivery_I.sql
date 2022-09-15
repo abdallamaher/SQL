15c95c1a9bcd814ca707342e15276368ae0ef29c
Abdallah Maher <abdallahmaher@Abdallahs-MacBook-Air.local>
2022-09-15 18:07:52 +0200

    1173._Immediate_Food_Delivery_I.sql

---
+++ 1173._Immediate_Food_Delivery_I.sql
@@ -0,0 +1,24 @@
+# Write your MySQL query statement below
+/*
+INPUT
+-----
+
+
+OUTPUT
+------
+
+
+CONSTRAINS
+-----------
+
+
+*/
+
+select 
+round(
+    sum(order_date = customer_pref_delivery_date) / 
+    count(*) * 
+    100,
+    2
+) as immediate_percentage
+from Delivery
