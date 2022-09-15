70576ede5c802d152745c3bf1b31349f3023b9ae
Abdallah Maher <abdallahmaher@Abdallahs-MacBook-Air.local>
2022-09-15 18:10:52 +0200

    1173._Immediate_Food_Delivery_I.sql

--- 1173._Immediate_Food_Delivery_I.sql
+++ 1173._Immediate_Food_Delivery_I.sql
@@ -13,7 +13,24 @@ CONSTRAINS
 
 
 */
+-- 816 ms
+select 
+round(
+    (
+    select count(*)
+    from Delivery
+    where order_date = customer_pref_delivery_date
+    )
+    / 
+    (
+        select count(*)
+        from Delivery
+    )
+    * 100, 
+    2
+) as immediate_percentage
 
+-- 886 ms
 select 
 round(
     sum(order_date = customer_pref_delivery_date) / 
