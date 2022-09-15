1c102826334b2c0592f0dab48df9d4f66c586fc3
Abdallah Maher <abdallahmaher@Abdallahs-MacBook-Air.local>
2022-09-15 17:59:18 +0200

    1148._Article_Views_I.sql

---
+++ 1148._Article_Views_I.sql
@@ -0,0 +1,19 @@
+# Write your MySQL query statement below
+/*
+INPUT
+-----
+
+
+OUTPUT
+------
+
+
+CONSTRAINS
+-----------
+
+
+*/
+select distinct author_id as id
+from Views
+where author_id = viewer_id
+order by id
